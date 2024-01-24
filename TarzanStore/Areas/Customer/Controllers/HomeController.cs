9f3a319023983f61f94de1f15a409b42ac66720e
Yazan <Yazan@DESKTOP-OSQTMG0>
2024-01-25 02:35:56 +0300

    Home and Details Page

--- TarzanStore/Areas/Customer/Controllers/HomeController.cs
+++ TarzanStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Tarzan.DataAccess.Repository.IRepository;
 using Tarzan.Models;
+using Tarzan.Models.ViewModels;
 
 namespace TarzanStore.Areas.Customer.Controllers
 {
@@ -8,15 +10,22 @@ namespace TarzanStore.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id== productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
